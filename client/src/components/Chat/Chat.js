ca59ceee06e26f6be17be9398bb51d34de35127d
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-07 16:40:02 +0900

    add effect hook

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+
+import io from 'socket.io-client';
 
 import './Chat.css';
 
-const Chat = () => (
-    <h1>Chat</h1>
-);
+const Chat = ({ location }) => {
+    useEffect(() => {
+        const data = queryString.parse(location.search); //from react router
+
+        console.log(location.search);
+        console.log(data);
+    });
+    return (
+        <h1>chat</h1>
+    );
+};
 
 export default Chat;
