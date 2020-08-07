a57c586983fe013f1da37ae6ea0d9b4b8b5314b1
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-07 16:48:04 +0900

    불필요한 이벤트 호출 줄여줌

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,16 +5,23 @@ import io from 'socket.io-client';
 
 import './Chat.css';
 
+let socket; //empty variable to save data from client
+
 const Chat = ({ location }) => {
     const [name, setName] = useState(''); //state
     const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000'; //배포 후 이부분 바꿈 (서버로 도착했을때 주소)
 
     useEffect(() => {
         const {name, room} = queryString.parse(location.search); //destructured
 
+        socket = io(ENDPOINT);
+
         setName(name);
         setRoom(room);
-    });
+
+        console.log(socket);
+    }, [ENDPOINT, location.search]); //오직 rerander할때만 이펙트가 옴
     return (
         <h1>chat</h1>
     );
