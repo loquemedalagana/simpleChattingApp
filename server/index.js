e509bba4a273ec5645076327c8cad978d4461303
loquemedalagana <nadamasquetu012@gmail.com>
2020-08-08 16:08:14 +0900

    서버에서 유저 입장 처리

--- server/index.js
+++ server/index.js
@@ -20,10 +20,20 @@ io.on('connection', (socket) => { //클라에서 받아온 정보 (소켓)
 
         if(error) return callback(error);
 
-        //emit new event
-        
+        //emit new event(~가 들어왔습니다)
+        socket.emit('message', {user: '관리자', text: `${user.name}님, 어서오세요!`}); //새 유저에게
+        socket.broadcast.to(user.room).emit('message', {user: '관리자', text: `${user.name}님이 입장했습니다.`}); //모든 유저에게
 
         socket.join(user.room);
+
+        callback(); //에러 있을 때 프론트에 전달
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', {user: user.name, text: message}); //프엔에서 받아온 정보
+
+        callback();
     });
 
     //user is gone
